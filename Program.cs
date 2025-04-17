bd3eaa53b2d1b4b1824b860cd4edda93ad16e336
Gergana Ivanova <gslavcheva933@gmail.com>
2025-04-17 11:10:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace _9_klass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            int theBigest = 0;
+            int sum = 0;
+            int element = 0; 
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0;i < nums.Length; i++)
+            {
+                if (nums[i] > theBigest)
+                {
+                 theBigest = nums[i];
+                }
+                sum += nums[i];
+            }
+          
+        }
+    }
+}
